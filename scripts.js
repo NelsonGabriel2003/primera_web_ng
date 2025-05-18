56c17ac9de0a63f947ce71c22f9f46cce41fc50c
ngallauca@puce.edu.ec <ngallauca@puce.edu.ec>
2025-05-18 11:06:11 -0500

    Ejercicio con script

--- scripts.js
+++ scripts.js
@@ -1,41 +1,14 @@
-//  para acceder a elementos de html se usa getelementbyid
-// para acceder a elemento de forma singularde htmml que son con clases selectores se usa "query selector"
+var numero_uno=prompt("Ingresa un numero: ");
+var numero_dos=prompt("Ingresa un numero: ");
 
-// el concepto de WHILE primero evalua la condicion para ejecutar:
-/*
-var i=0;
-while(i<=10){
-    console.log("Estoy en la vuelta numero "+ i);
-    i=i+1;
+var resultado=numero_uno-numero_dos;
+if(resultado>0){
+    console.log("Es mayor a 0");
+    if(resultado%2==0){
+        console.log("Y tambien es un numero par");
+    }else{
+        console.log("Y tambien es un numero impar");
+    }
+}else{
+    console.log("Es menor o igual a  0")
 }
-
-*/
-//------------------------------------------------------------
-// El concepto de FOR es una estructura repetitiva por un
-//valor inicial,un condicion de parada,incremento :
-/*
-for(var i=0; i<10;i++){
-    console.log("Vuelta n* "+ i);
-}
-
-alert("Estamos en la vuelta numero "+ (i-1));
-*/
-//------------------------------------------------------------
-// El concepto de FOR es una estructura repetitiva pero que se 
-// evalua al menos una vez SIEMPRE :
-
-var i=0;
-do{
-    console.log("El numero de vuelta es: "+i);
-    i++;
-}
-while(i)
-
-let numero;
-do {
-    numero = parseInt(prompt("Introduce un número positivo:"));
-} while (numero <= 0);
-if(numero===int){
-    alert("Su numero es:"+ numero)
-}
-    
